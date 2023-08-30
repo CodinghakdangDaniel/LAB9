c996985880aa3297d49ded57f3f31a02c27dd721
CodinghakdangDaniel <junh2e97@gmail.com>
2023-08-30 13:10:42 +0900

    lab9

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// var firstName = "daniel";
+// var lastName = "Jo";
+
+// console.log(firstName);
+
+var x = "1";
+var y = "3";
+
+console.log(x + y);
+
+
+
+
+// console.log("Hello, Hakdang 1기!!");
